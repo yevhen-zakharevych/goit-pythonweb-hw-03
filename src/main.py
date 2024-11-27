d8479de0cb4cf1b5898fa9fcc98430c7d940f27b
Yevhen Zakharevych <yzakharevych@fieldcomplete.com>
2024-11-27 21:17:20 +0100

    homework

---
+++ src/main.py
@@ -0,0 +1,77 @@
+import json
+import mimetypes
+import pathlib
+from datetime import datetime
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.parse
+from jinja2 import Environment, FileSystemLoader
+
+
+class HttpHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        pr_url = urllib.parse.urlparse(self.path)
+        if pr_url.path == '/':
+            self.send_html_file('index.html')
+        elif pr_url.path == '/message':
+            self.send_html_file('message.html')
+        elif self.path == '/load':
+            with open('storage/data.json', 'r') as file:
+                messages = json.load(file)
+            env = Environment(loader=FileSystemLoader('.'))
+            template = env.get_template("load.html")
+            rendered_html = template.render(messages=messages)
+            with open("_load.html", "w", encoding='utf-8') as fh:
+                fh.write(rendered_html)
+            self.send_html_file('_load.html', 200)
+        else:
+            if pathlib.Path().joinpath(pr_url.path[1:]).exists():
+                self.send_static()
+            self.send_html_file('error.html', 404)
+
+    def send_html_file(self, filename, status=200):
+        self.send_response(status)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        with open(filename, 'rb') as fd:
+            self.wfile.write(fd.read())
+
+    def send_static(self):
+        self.send_response(200)
+        mt = mimetypes.guess_type(self.path)
+        if mt:
+            self.send_header("Content-type", mt[0])
+        else:
+            self.send_header("Content-type", 'text/plain')
+        self.end_headers()
+        with open(f'.{self.path}', 'rb') as file:
+            self.wfile.write(file.read())
+
+    def do_POST(self):
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        data_parse = urllib.parse.unquote_plus(data.decode())
+        data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
+        timestamp = datetime.now().isoformat()
+
+        with open('storage/data.json', 'r') as file:
+            storage = json.load(file)
+        storage[timestamp] = data_dict
+
+        with open('storage/data.json', 'w') as file:
+            json.dump(storage, file, indent=4)
+
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.end_headers()
+
+
+def run(server_class=HTTPServer, handler_class=HttpHandler):
+    server_address = ('', 8000)
+    http = server_class(server_address, handler_class)
+    try:
+        http.serve_forever()
+    except KeyboardInterrupt:
+        http.server_close()
+
+
+if __name__ == '__main__':
+    run()
